1abdc149502ac5dc17aed2a5d7d0fd7591829266
eoqna <eoqna@naver.com>
2024-08-28 22:34:33 +0900

    Next JS의 데이터 fetching 방법 학습

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,10 +2,21 @@ export const metadata = {
   title: "Home",
 };
 
-export default function Page() {
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
+
+export async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+};
+
+export default async function Page() {
+  const movies = await getMovies();
+
   return (
     <div>
-      <h1>Hello!</h1>
+      {JSON.stringify(movies)}
     </div>
   );
 };
