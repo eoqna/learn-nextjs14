6b7e9f6d95afe322cc0ba09a444135506f712140
eoqna <eoqna@naver.com>
2024-09-03 22:03:39 +0900

    페이지 style 적용 및 component 분리

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,11 +1,11 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error("Somthing broke...");
-  // const response = await fetch(`${API_URL}/${id}/videos`);
-  // const json = await response.json();
-  // return json;
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  // throw new Error("Somthing broke...");
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  const json = await response.json();
+  return json;
 };
 
 export default async function MovieVedios({id}: {id: string} ) {
